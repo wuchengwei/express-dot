4142529df0f7e1b318bfa2fbd754bd4d37156f5f
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-03-02 02:15:20 +0000

    setInterpolationSymbols

--- express-dot.js
+++ express-dot.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var path = require('path');
 var doT = require('dot');
 var async = require('async');
-var path = require('path'); 
 
 var _cache = {};
 var _partialsCache = {};
@@ -78,6 +77,30 @@ exports.setTemplateSettings = function(settings) {
 exports.getTemplateSettings = function(settings) {
   return doT.templateSettings;
 };
+
+exports.setInterpolationSymbols = function(startSymbol, endSymbol) {
+  for (option in doT.templateSettings) {
+    var value = doT.templateSettings[option];
+    if (value instanceof RegExp) {
+      var regexStr = value.toString()
+      regexStr = regexStr
+                  .replace(/\\\{\\\{/g, _regexStr(startSymbol))
+                  .replace(/\\\}\\\}/g, _regexStr(endSymbol));
+      doT.templateSettings[option] = _toRegExp(regexStr);
+    }
+  }
+  console.log(doT.templateSettings);
+};
+
+function _regexStr(str) {
+  return str.replace(/([^a-z0-9_])/ig, '\\$1')
+}
+
+var regexpStringPattern = /^\/(.*)\/([gimy]*)$/;
+function _toRegExp(str) {
+    var rx = str.match(regexpStringPattern);
+    if (rx) return new RegExp(rx[1], rx[2]);
+}
 
 exports.__express = function(filename, options, cb) {
   'use strict';
